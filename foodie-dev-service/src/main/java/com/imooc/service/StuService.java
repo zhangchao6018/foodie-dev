8c94a5f8a5a64268c8746a2f0235460d024388b0
zhangchao <18910146715@163.com>
2020-03-05 17:56:21 +0800

    事务的传播

--- foodie-dev-service/src/main/java/com/imooc/service/StuService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/StuService.java
@@ -8,5 +8,14 @@ import com.imooc.pojo.Stu;
  * @Date: 2020-02-17 11:38
  **/
 public interface StuService {
-    Stu getStuInfo(int id);
+    public Stu getStuInfo(int id);
+
+    public void saveStu();
+
+    public void updateStu(int id);
+
+    public void deleteStu(int id);
+
+    public void saveParent();
+    public void saveChildren();
 }
