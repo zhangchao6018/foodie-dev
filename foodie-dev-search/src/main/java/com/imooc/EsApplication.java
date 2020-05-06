5d63543c20b63291c1f839ebc88c4faf12b44fcc
zhangchao <18910146715@163.com>
2020-05-06 08:16:14 +0800

    跳过spring-security

--- foodie-dev-search/src/main/java/com/imooc/EsApplication.java
+++ foodie-dev-search/src/main/java/com/imooc/EsApplication.java
@@ -3,7 +3,10 @@ package com.imooc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//todo 开发阶段跳过认证
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 public class EsApplication {
 
     public static void main(String[] args) {
